8d35fe20b97b92500124be7d2252d58fe9d0e234
Abar4 <115609195+Abar4@users.noreply.github.com>
2024-06-15 09:58:41 +0530

    Add files via upload

---
+++ src/component/FooterComponent.jsx
@@ -0,0 +1,15 @@
+
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>
+                All rights reserve 2023 by our company
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
