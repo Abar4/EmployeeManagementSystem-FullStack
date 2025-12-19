300f8d5fec02c528219565b73abbea09089fdb42
Abarna <727821tuec001@skct.edu.in>
2025-12-19 19:55:03 +0530

    grafana

--- pushMetrics.js
+++ pushMetrics.js
@@ -1,9 +1,32 @@
-import { pushMetrics } from "./server/grafanaMetrics.js";
+import axios from 'axios';
+import fs from 'fs';
 
-try {
-  await pushMetrics();
-  process.exit(0);
-} catch (err) {
-  console.error(err);
+const metricsData = fs.readFileSync('./metrics.txt', 'utf8'); // or generate dynamically
+const GRAFANA_URL = 'https://prometheus-prod-43-prod-ap-south-1.grafana.net/api/prom/push';
+const GRAFANA_API_KEY = process.env.GRAFANA_API_KEY;
+
+if (!GRAFANA_API_KEY) {
+  console.error('Error: GRAFANA_API_KEY not set');
   process.exit(1);
 }
+
+async function pushMetrics() {
+  try {
+    const response = await axios.post(GRAFANA_URL, metricsData, {
+      headers: {
+        'Authorization': `Bearer ${GRAFANA_API_KEY}`,
+        'Content-Type': 'text/plain',
+      },
+    });
+    console.log('Metrics pushed successfully:', response.data);
+  } catch (error) {
+    if (error.response) {
+      console.error('Grafana API error:', error.response.status, error.response.data);
+    } else {
+      console.error('Request error:', error.message);
+    }
+    process.exit(1);
+  }
+}
+
+pushMetrics();
